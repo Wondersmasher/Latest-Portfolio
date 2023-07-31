6a7f2c9608256016283406e8e5023e0fbee4b075
wondersmasher <wonderfulakinniyi@gmai.com>
2023-07-31 14:50:56 +0100

    Working on the hero section.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Hero = () => {
-  return <></>;
+  return (
+    <div className=" flex flex-col gap-8 justify-center items-center py-64 bg-[url('/Profile-Picture.png')] bg-no-repeat bg-contain bg-blend-overlay bg-black">
+      <p className="font-bold text-white text-4xl text-center ">
+        Hi there, I am{" "}
+        <span className="text-primary">
+          Ezekiel Akinniyi <br />
+        </span>{" "}
+      </p>
+      <p className="font-bold text-white text-3xl text-center ">
+        A <span className="text-primary">FrontEnd Developer</span>
+      </p>
+      <p className="text-gray">
+        I craft responsive websites where technologies meets creativity
+      </p>
+      <div className="flex justify-center items-center gap-4">
+        <div className="animate-bounce-slow hover:animate-none">
+          <div className="text-white border-2 px-4 py-2 border-white cursor-pointer animate-pulse-slow hover:animate-none">
+            Resume
+          </div>
+        </div>
+        <div className="text-white border-2 px-4 py-2 border-white cursor-pointer animate-pulse-slow hover:animate-none">
+          Contact me!!
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,27 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 const NavBar = () => {
-  return <nav></nav>;
+  return (
+    <nav className="flex justify-between items-center px-12 py-6">
+      <div className="flex justify-center items-center gap-2">
+        <p className=" font-bold text-2xl">{"<Ezekiel/>"}</p>
+      </div>
+      <ul className="flex justify-center items-center gap-4">
+        <li className="cursor-pointer ">
+          <span className="text-primary">#</span>home
+        </li>
+        <li className="cursor-pointer ">
+          <span className="text-primary">#</span>about
+        </li>
+        <li className="cursor-pointer ">
+          <span className="text-primary">#</span>projects
+        </li>
+        <li className="cursor-pointer ">
+          <span className="text-primary">#</span>contact
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
