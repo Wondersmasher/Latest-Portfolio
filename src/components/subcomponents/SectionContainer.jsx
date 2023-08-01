be02f39ef60d54a204e6e80b94a29ebb15ac0105
wondersmasher <wonderfulakinniyi@gmai.com>
2023-08-01 21:10:02 +0100

    some minor changes to ptop

--- src/components/subcomponents/SectionContainer.jsx
+++ src/components/subcomponents/SectionContainer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hr from "./Hr";
-const SectionContainer = ({ children, title }) => {
+const SectionContainer = ({ children, title, pt = "pt-16" }) => {
   return (
-    <section className="px-6 pt-16  lg:px-12 xl:px-32  flex flex-col gap-2 justify-center">
-      <div className="flex gap-2 justify-center items-center mb-12">
-        <p className=" font-bold text-5xl text-gray ">
+    <section
+      className={`px-6  ${pt}  lg:px-12 xl:px-32  flex flex-col gap-2 justify-center `}
+    >
+      <div className="flex gap-2 justify-center items-center mb-6 lg:mb-12">
+        <p className=" font-bold text-2xl md:text-3xl lg:text-5xl text-gray font-sacramento ">
           <span className="text-primary">#</span>
           {title}
         </p>
