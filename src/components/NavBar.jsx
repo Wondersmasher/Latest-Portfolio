f1383644d5957bf3c3bdc81f69e979744206e58b
wondersmasher <wonderfulakinniyi@gmai.com>
2023-08-01 08:45:11 +0100

    added audio tag

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NavBar = () => {
+  const [play, setPlay] = useState(true);
   return (
-    <nav className="flex justify-between items-center px-12 py-6">
+    <nav className="flex justify-between items-center px-6 py-3 md:py-6 md:px-12 lg:px-32">
       <div className="flex justify-center items-center gap-2">
-        <p className=" font-bold text-2xl">{"<Ezekiel/>"}</p>
+        <p className=" font-bold text-lg md:text-xl lg:text-2xl">
+          {"<Ezekiel/>"}
+        </p>
+        <audio src="/audio/leva-eternity.mp3" loop></audio>
       </div>
-      <ul className="flex justify-center items-center gap-4">
+      <ul className="md:flex justify-center items-center gap-4 hidden md:visible">
         <li className="cursor-pointer ">
           <span className="text-primary">#</span>home
         </li>
