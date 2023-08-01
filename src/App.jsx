fac40156993db71c27993b5fdc8581b6f2aeb311
wondersmasher <wonderfulakinniyi@gmai.com>
2023-08-01 21:06:44 +0100

    Added contact me component and implemented it

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,17 @@ import Hero from "./components/Hero";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import ContactMe from "./components/ContactMe";
 
 const App = () => {
   return (
     <div className="bg-black min-h-[100vh] font-firaCode text-white w-full">
       <NavBar />
       <Hero />
-      <Projects />
       <AboutMe />
       <Skills />
       <Projects />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptates,
-      enim illo quis aperiam repellat aliquam a! Adipisci quisquam iusto magni
-      rerum perspiciatis sapiente repudiandae provident aspernatur corporis
-      earum, fugit eligendi ullam? Dolorum vitae veniam nihil maxime adipisci
-      dolor ratione neque quae, doloremque ea, atque, veritatis est voluptate
-      quas illo accusamus voluptatibus molestiae cumque saepe porro itaque
-      possimus! Pariatur, minima. Hic rerum ipsa porro blanditiis debitis
-      numquam molestias enim quam inventore consequatur placeat nemo incidunt
-      eos cupiditate perspiciatis laborum, velit ipsum illo alias! Accusantium
-      aliquam aperiam iusto fugit ipsam quidem, id quibusdam accusamus cumque
-      laudantium vero repudiandae velit vitae reprehenderit voluptate rem,
-      eveniet voluptates. Temporibus sunt quam quo doloremque natus mollitia
-      fugit expedita ex officiis non beatae, eveniet assumenda quos inventore
-      dolorem ducimus laboriosam, eius laborum nostrum corrupti ipsa magnam nam?
-      A cumque maxime esse cum, mollitia tempora asperiores tenetur dolore vero?
-      Quam ipsum excepturi recusandae suscipit molestias, tempore architecto
-      molestiae sit numquam sed ea dolore harum ad provident laborum facilis
-      animi non dolor, porro autem optio odio sunt! Ea ut, quibusdam illum
-      excepturi, necessitatibus est optio repellendus, commodi esse quos
-      expedita possimus quasi vero. Quisquam magni aliquam incidunt reiciendis,
-      dolore cum quae ea culpa, officiis ut commodi exercitationem sunt.
+      <ContactMe />
     </div>
   );
 };
---
+++ src/components/ContactMe.jsx
@@ -0,0 +1,115 @@
+import React, { useRef } from "react";
+import SectionContainer from "./subcomponents/SectionContainer";
+import Texts from "./subcomponents/Texts";
+import emailjs from "emailjs-com";
+
+const ContactMe = () => {
+  const form = useRef();
+  const input1 = useRef();
+  const input2 = useRef();
+  const input3 = useRef();
+  const input4 = useRef();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validEmail = emailRegex.test(input2.current.value);
+    try {
+      if (
+        !input1.current.value ||
+        !input2.current.value ||
+        !input3.current.value ||
+        !input4.current.value
+      ) {
+        alert("Incomplete credentials");
+        return;
+      }
+      if (!validEmail) {
+        alert("Please enter a valid E-mail");
+        return;
+      }
+      await emailjs.sendForm(
+        "service_n4hy5rv",
+        "template_6yikhke",
+        form.current,
+        "LYD2PjwK_ChypiseY"
+      );
+      alert(
+        "Email Sent 📨./n Thank you for contacting me 🤝./n You will be responded to immediately"
+      );
+    } catch (error) {
+      console.error(`Failed to send. Error is ${error}`);
+      alert(
+        `❌❌❌❌❌❌ /n There was an error sending your message. /n ${error}`
+      );
+    }
+  };
+
+  return (
+    <SectionContainer title="contactMe">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-8 pb-12">
+        <Texts size="lg">
+          <p>
+            I'm excited to hear from you and explore the endless possibilities
+            of frontend development.
+          </p>
+          <p>
+            Whether you have a project in mind or need a skilled developer to
+            breathe life into your ideas, don't hesitate to reach out! Use the
+            form below to get in touch, and let's create something extraordinary
+            together.
+          </p>
+          <p>Looking forward to connecting with you!</p>
+        </Texts>
+        <form ref={form} className="flex flex-col gap-3 lg:w-[50%] w-[100%] ">
+          <div className="md:flex grid gap-4">
+            <input
+              ref={input1}
+              className={`border-[1px]    focus:outline-none w-[100%] p-1 bg-black `}
+              type="text"
+              name="name"
+              id="1"
+              placeholder="Full Name"
+            />
+            <input
+              ref={input2}
+              className={`  border-[1px]   focus:outline-none w-[100%] p-1 bg-black`}
+              type="email"
+              name="email"
+              id="2"
+              placeholder="E-mail"
+              required
+            />
+          </div>
+          <input
+            ref={input3}
+            className={` border-[1px]   bg-black focus:outline-none w-[100%] p-1 `}
+            type="text"
+            name="subject"
+            id="3"
+            placeholder="Subject"
+          />
+          <textarea
+            ref={input4}
+            className={`bg-black h-32 border-[1px]    focus:outline-none mx-auto w-[100%] p-1`}
+            type="text"
+            name="message"
+            id="4"
+            placeholder="Your Message"
+          />
+          <div>
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className=" border-[1px]   p-2  mt-3 font-bold font-sacramento"
+            >
+              Send Message
+            </button>
+          </div>
+        </form>
+      </div>
+    </SectionContainer>
+  );
+};
+
+export default ContactMe;
