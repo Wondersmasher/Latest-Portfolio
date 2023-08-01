3c99c31839f07dbe11631af07e5e5879f6d28672
wondersmasher <wonderfulakinniyi@gmai.com>
2023-08-01 08:44:24 +0100

    starting out skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SectionContainer from "./subcomponents/SectionContainer";
+
+const Skills = () => {
+  return <SectionContainer title="skills" />;
+};
+
+export default Skills;
